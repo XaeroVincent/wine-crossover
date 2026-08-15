2282e93cd95aea89fba133ccaab81f832fbc046e
Vince <31942676+XaeroVincent@users.noreply.github.com>
2026-08-14 22:07:12 -0700

    Add Steam helper script
    Add Steam helper script

---
+++ winesteam.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#WINEPREFIX=$HOME/.wine_steam wine reg.exe ADD "HKEY_CURRENT_USER\Software\Wine\DllOverrides" /v "gameoverlayrenderer" /t "REG_SZ" /d "" /f
+#WINEPREFIX=$HOME/.wine_steam wine reg.exe ADD "HKEY_CURRENT_USER\Software\Wine\DllOverrides" /v "gameoverlayrenderer64" /t "REG_SZ" /d "" /f
+#WINEPREFIX=$HOME/.wine_steam wine reg.exe ADD "HKEY_CURRENT_USER\Software\Wine\AppDefaults\steamwebhelper.exe\DllOverrides" /v "d3d11" /t "REG_SZ" /d "builtin" /f
+#WINEPREFIX=$HOME/.wine_steam wine reg.exe ADD "HKEY_CURRENT_USER\Software\Wine\AppDefaults\steamwebhelper.exe\DllOverrides" /v "dxgi" /t "REG_SZ" /d "builtin" /f
+#WINEPREFIX=$HOME/.wine_steam winetricks -q corefonts nocrashdialog
+
+export WINEDEBUG="-all"
+export PULSE_LATENCY_MSEC=60
+export WINE_LARGE_ADDRESS_AWARE=1
+export WINEPREFIX=$HOME/.wine_steam
+#export WINE_D3D_CONFIG="renderer=vulkan"
+
+/usr/local/bin/wine "$HOME/.wine_steam/drive_c/Program Files (x86)/Steam/steam.exe" -no-cef-sandbox -cef-disable-sandbox -cef-disable-gpu -console
